62dca88f097cf3b48c1e0669e0a02c2ff67d032e
Muhammad Novel <66077593+mnovel@users.noreply.github.com>
2023-09-08 14:09:42 +0700

    Tes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
             <div class="col">
                 <div class="card card-info">
                     <div class="card-header">
-                        <h3 class="card-title">List Supplier</h3>
+                        <h3 class="card-title">List Tes</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
